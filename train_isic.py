f66717e16fbe3cab7189dd6497342c892b0d1d81
ienesb <63236773+ienesb@users.noreply.github.com>
2021-09-06 00:17:41 +0300

    Update train_isic.py

--- train_isic.py
+++ train_isic.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from test_isic import mean_dice_np, mean_iou_np
 import os
+import shutil
 
 
 def structure_loss(pred, mask):
@@ -24,7 +25,7 @@ def structure_loss(pred, mask):
     return (wbce + wiou).mean()
 
 
-def train(train_loader, model, optimizer, epoch, best_loss):
+def train(train_loader, model, optimizer, epoch, best_loss, n, checkpoint):
     model.train()
     loss_record2, loss_record3, loss_record4 = AvgMeter(), AvgMeter(), AvgMeter()
     accum = 0
@@ -35,6 +36,7 @@ def train(train_loader, model, optimizer, epoch, best_loss):
         gts = Variable(gts).cuda()
 
         # ---- forward ----
+        
         lateral_map_4, lateral_map_3, lateral_map_2 = model(images)
 
         # ---- loss function ----
@@ -61,16 +63,21 @@ def train(train_loader, model, optimizer, epoch, best_loss):
                   '[lateral-2: {:.4f}, lateral-3: {:0.4f}, lateral-4: {:0.4f}]'.  
                   format(datetime.now(), epoch, opt.epoch, i, total_step,
                          loss_record2.show(), loss_record3.show(), loss_record4.show()))
+                      
+    save_path = os.path.join(opt.train_save, 'TransFuse_ISKEMI_' + str(epoch + n) + '_Epoch.pth')
+    os.makedirs("/content/TransFuse/snapshots", exist_ok=True)
 
-    save_path = 'snapshots/{}/'.format(opt.train_save)
-    os.makedirs(save_path, exist_ok=True)
-    if (epoch+1) % 1 == 0:
-        meanloss = test(model, opt.test_path)
-        if meanloss < best_loss:
-            print('new best loss: ', meanloss)
-            best_loss = meanloss
-            torch.save(model.state_dict(), save_path + 'TransFuse-%d.pth' % epoch)
-            print('[Saving Snapshot:]', save_path + 'TransFuse-%d.pth'% epoch)
+    meanloss = test(model, opt.test_path)
+    if meanloss < best_loss:
+        print('mean loss: ', meanloss)
+        best_loss = meanloss
+
+    if epoch % checkpoint == 0 or epoch == total_step:
+        torch.save(model.state_dict(), save_path)
+        shutil.copy(save_path, "/content/drive/MyDrive/İNAN/SağlıktaYapayZeka/TransFuse/new_dataset/ISKEMI/" + 'TransFuse_ISKEMI_' + 
+              str(epoch + n) + '_Epoch.pth')
+        print('[Saving Snapshot:]', save_path)
+
     return best_loss
 
 
@@ -79,68 +86,76 @@ def test(model, path):
     model.eval()
     mean_loss = []
 
-    for s in ['val', 'test']:
-        image_root = '{}/data_{}.npy'.format(path, s)
-        gt_root = '{}/mask_{}.npy'.format(path, s)
-        test_loader = test_dataset(image_root, gt_root)
+    image_root = '{}/data_iskemi_test.npy'.format(path)
+    gt_root = '{}/mask_iskemi_test.npy'.format(path)
+    test_loader = test_dataset(image_root, gt_root)
 
-        dice_bank = []
-        iou_bank = []
-        loss_bank = []
-        acc_bank = []
+    dice_bank = []
+    iou_bank = []
+    loss_bank = []
+    acc_bank = []
 
-        for i in range(test_loader.size):
-            image, gt = test_loader.load_data()
-            image = image.cuda()
+    for i in range(test_loader.size):
+        image, gt = test_loader.load_data()
+        image = image.cuda()
 
-            with torch.no_grad():
-                _, _, res = model(image)
-            loss = structure_loss(res, torch.tensor(gt).unsqueeze(0).unsqueeze(0).cuda())
+        with torch.no_grad():
+            _, _, res = model(image)
+        loss = structure_loss(res, torch.tensor(gt).unsqueeze(0).unsqueeze(0).cuda())
 
-            res = res.sigmoid().data.cpu().numpy().squeeze()
-            gt = 1*(gt>0.5)            
-            res = 1*(res > 0.5)
+        res = res.sigmoid().data.cpu().numpy().squeeze()
+        gt = 1*(gt>0.5)            
+        res = 1*(res > 0.5)
 
-            dice = mean_dice_np(gt, res)
-            iou = mean_iou_np(gt, res)
-            acc = np.sum(res == gt) / (res.shape[0]*res.shape[1])
+        dice = mean_dice_np(gt, res)
+        iou = mean_iou_np(gt, res)
+        acc = np.sum(res == gt) / (res.shape[0]*res.shape[1])
 
-            loss_bank.append(loss.item())
-            dice_bank.append(dice)
-            iou_bank.append(iou)
-            acc_bank.append(acc)
-            
-        print('{} Loss: {:.4f}, Dice: {:.4f}, IoU: {:.4f}, Acc: {:.4f}'.
-            format(s, np.mean(loss_bank), np.mean(dice_bank), np.mean(iou_bank), np.mean(acc_bank)))
+        loss_bank.append(loss.item())
+        dice_bank.append(dice)
+        iou_bank.append(iou)
+        acc_bank.append(acc)
+        
+    print('Test Loss: {:.4f}, Dice: {:.4f}, IoU: {:.4f}, Acc: {:.4f}'.
+        format(np.mean(loss_bank), np.mean(dice_bank), np.mean(iou_bank), np.mean(acc_bank)))
 
-        mean_loss.append(np.mean(loss_bank))
+    mean_loss.append(np.mean(loss_bank))
 
     return mean_loss[0] 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epoch', type=int, default=25, help='epoch number')
+    parser.add_argument('--epoch', type=int, default=50, help='epoch number')
     parser.add_argument('--lr', type=float, default=7e-5, help='learning rate')
     parser.add_argument('--batchsize', type=int, default=16, help='training batch size')
     parser.add_argument('--grad_norm', type=float, default=2.0, help='gradient clipping norm')
     parser.add_argument('--train_path', type=str,
-                        default='data/', help='path to train dataset')
+                        default='/content/ISKEMI_npy_files', help='path to train dataset')
     parser.add_argument('--test_path', type=str,
-                        default='data/', help='path to test dataset')
-    parser.add_argument('--train_save', type=str, default='TransFuse_S')
+                        default='/content/ISKEMI_npy_files', help='path to test dataset')
+    parser.add_argument('--train_save', type=str, default='/content/TransFuse/snapshots')
     parser.add_argument('--beta1', type=float, default=0.5, help='beta1 of adam optimizer')
     parser.add_argument('--beta2', type=float, default=0.999, help='beta2 of adam optimizer')
+    parser.add_argument('--pretrained', type=int, default=1)
+    parser.add_argument('--checkpoint', type=int, default=1)
 
     opt = parser.parse_args()
 
     # ---- build models ----
-    model = TransFuse_S(pretrained=True).cuda()
+    model = TransFuse_S(pretrained=opt.pretrained).cuda()
+    n=0
+    if opt.pretrained:
+        dirlist = [v.split(".")[0].split("_")[-2] for v in os.listdir("/content/drive/MyDrive/İNAN/SağlıktaYapayZeka/TransFuse/new_dataset/ISKEMI")]
+        n = max([int(i) for i in dirlist])
+        print(n)
+        model_dir = "/content/drive/MyDrive/İNAN/SağlıktaYapayZeka/TransFuse/new_dataset/ISKEMI/TransFuse_ISKEMI_"+str(n)+"_Epoch.pth"
+        model.load_state_dict(torch.load(model_dir))
     params = model.parameters()
     optimizer = torch.optim.Adam(params, opt.lr, betas=(opt.beta1, opt.beta2))
      
-    image_root = '{}/data_train.npy'.format(opt.train_path)
-    gt_root = '{}/mask_train.npy'.format(opt.train_path)
+    image_root = '{}/data_iskemi_train.npy'.format(opt.train_path)
+    gt_root = '{}/mask_iskemi_train.npy'.format(opt.train_path)
 
     train_loader = get_loader(image_root, gt_root, batchsize=opt.batchsize)
     total_step = len(train_loader)
@@ -149,4 +164,5 @@ if __name__ == '__main__':
 
     best_loss = 1e5
     for epoch in range(1, opt.epoch + 1):
-        best_loss = train(train_loader, model, optimizer, epoch, best_loss)
+        best_loss = train(train_loader, model, optimizer, epoch, best_loss, n, opt.checkpoint)
+        

92bfb461b647756ad8cb7f99a198f95091e26ffb
gencf <79080187+gencf@users.noreply.github.com>
2021-09-07 07:17:59 +0300

    Update test_isic.py

--- test_isic.py
+++ test_isic.py
@@ -5,11 +5,12 @@ import os, argparse
 from lib.TransFuse import TransFuse_S
 from utils.dataloader import test_dataset
 import imageio
+import cv2
 
 def mean_iou_np(groundTruthMask, predictedMask):
     
     # 1 ve 2 maskelerini ayrı ayrı oluşturuyoruz (0 ve 255 değerlerinin olduğu birer maske olarak)
-    mask1 = np.where(groundtruthMask == 255, 255, 0).astype(np.uint8)
+    mask1 = np.where(0 == 255, 255, 0).astype(np.uint8)
     mask2 = np.where(groundtruthMask == 255, 255, 0).astype(np.uint8)      
 
     # İki sınıfı ayrı ayrı dilate ve erode ediyoruz
@@ -59,8 +60,7 @@ def mean_dice_np(y_true, y_pred, **kwargs):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test_path', type=str,
-                        default='/kaggle/working/npy_files', help='path to test dataset')
+    parser.add_argument('--test_path', type=str, default='/kaggle/working/npy_files', help='path to test dataset')
     parser.add_argument('--save_path', type=str, default="/kaggle/working/results", help='path to save inference segmentation')
     parser.add_argument('--epoch', type=int, default=50, help='epoch for inference')
     parser.add_argument('--model_path', type=str, default='/kaggle/working/models', help='path for testing models')
@@ -89,13 +89,14 @@ if __name__ == '__main__':
 
     for i in range(test_loader.size):
         image, gt = test_loader.load_data()
-        iou = mean_iou_np(gt, res)
-
+        groundtruthMask = gt.copy()
+        
         gt = 1*(gt>0.5)
         image = image.cuda()
 
         with torch.no_grad():
             _, _, res = model(image)
+            predictedMask = res.copy()
 
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = 1*(res > 0.5)
@@ -105,6 +106,7 @@ if __name__ == '__main__':
             imageio.imwrite(opt.save_path+'/'+str(i)+'_gt.jpg', gt)
 
         dice = mean_dice_np(gt, res)
+        iou = mean_iou_np(groundtruthMask, predictedMask)
         acc = np.sum(res == gt) / (res.shape[0]*res.shape[1])
 
         acc_bank.append(acc)

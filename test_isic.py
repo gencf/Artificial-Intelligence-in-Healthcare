a9ccaac823b9768f1915b98feefb230be86fd597
gencf <79080187+gencf@users.noreply.github.com>
2021-09-07 06:58:43 +0300

    Update test_isic.py

--- test_isic.py
+++ test_isic.py
@@ -89,6 +89,8 @@ if __name__ == '__main__':
 
     for i in range(test_loader.size):
         image, gt = test_loader.load_data()
+        iou = mean_iou_np(gt, res)
+
         gt = 1*(gt>0.5)
         image = image.cuda()
 
@@ -103,7 +105,6 @@ if __name__ == '__main__':
             imageio.imwrite(opt.save_path+'/'+str(i)+'_gt.jpg', gt)
 
         dice = mean_dice_np(gt, res)
-        iou = mean_iou_np(gt, res)
         acc = np.sum(res == gt) / (res.shape[0]*res.shape[1])
 
         acc_bank.append(acc)

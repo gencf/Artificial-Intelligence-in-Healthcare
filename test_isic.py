2b794aecb0acc931d5d0c02bd9a688149c30cc4b
gencf <79080187+gencf@users.noreply.github.com>
2021-09-07 07:19:01 +0300

    Update test_isic.py

--- test_isic.py
+++ test_isic.py
@@ -5,11 +5,12 @@ import os, argparse
 from lib.TransFuse import TransFuse_S
 from utils.dataloader import test_dataset
 import imageio
+import cv2
 
 def mean_iou_np(groundTruthMask, predictedMask):
     
     # 1 ve 2 maskelerini ayrı ayrı oluşturuyoruz (0 ve 255 değerlerinin olduğu birer maske olarak)
-    mask1 = np.where(groundtruthMask == 255, 255, 0).astype(np.uint8)
+    mask1 = np.where(0 == 255, 255, 0).astype(np.uint8)
     mask2 = np.where(groundtruthMask == 255, 255, 0).astype(np.uint8)      
 
     # İki sınıfı ayrı ayrı dilate ve erode ediyoruz
@@ -88,13 +89,14 @@ if __name__ == '__main__':
 
     for i in range(test_loader.size):
         image, gt = test_loader.load_data()
-        iou = mean_iou_np(gt, res)
+        groundtruthMask = gt.copy()
         
         gt = 1*(gt>0.5)
         image = image.cuda()
 
         with torch.no_grad():
             _, _, res = model(image)
+            predictedMask = res.copy()
 
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = 1*(res > 0.5)
@@ -104,6 +106,7 @@ if __name__ == '__main__':
             imageio.imwrite(opt.save_path+'/'+str(i)+'_gt.jpg', gt)
 
         dice = mean_dice_np(gt, res)
+        iou = mean_iou_np(groundtruthMask, predictedMask)
         acc = np.sum(res == gt) / (res.shape[0]*res.shape[1])
 
         acc_bank.append(acc)

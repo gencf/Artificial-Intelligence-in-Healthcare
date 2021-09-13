e880e78fda2df362028b36302282396eabdc182c
gencf <79080187+gencf@users.noreply.github.com>
2021-09-14 01:55:20 +0300

    Update test_isic.py

--- test_isic.py
+++ test_isic.py
@@ -14,8 +14,7 @@ def mean_iou_np(gt_path, y_pred, **kwargs):
     groundtruthMask = cv2.imread(gt_path, 0)
     y_pred *= 255
     shape = groundtruthMask.shape[:2]
-    print(shape, y_pred.shape, type(y_pred), np.unique(y_pred))
-    predictedMask = cv2.resize(y_pred.astype('float32'), shape)
+    predictedMask = cv2.resize(y_pred.astype('float32'), shape[::-1])
     iou = calculateIoU(groundtruthMask, predictedMask, showSteps = False)
     return iou
 

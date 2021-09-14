8fc418005d19fc3610a8b636d2f0f763651147a8
gencf <79080187+gencf@users.noreply.github.com>
2021-09-14 07:57:50 +0300

    Update test_isic.py

--- test_isic.py
+++ test_isic.py
@@ -9,26 +9,15 @@ import cv2
 
 from IoU import calculateIoU
 
-def mean_iou_np(gt_path, y_pred, **kwargs):
+def mean_iou_np(gt_path, res_path, **kwargs):
     
     groundtruthMask = cv2.imread(gt_path, 0)
-    y_pred *= 255
+    predictedMask = cv2.imread(res_path, 0)    
     shape = groundtruthMask.shape[:2]
-    predictedMask = cv2.resize(y_pred.astype('float32'), shape[::-1])
+    predictedMask = cv2.resize(predictedMask, shape[::-1])
+    predictedMask = 255*predictedMask
     iou = calculateIoU(groundtruthMask, predictedMask, showSteps = False)
     return iou
-
-    """
-    compute mean iou for binary segmentation map via numpy
-    """
-    # axes = (0, 1) 
-    # intersection = np.sum(np.abs(y_pred * y_true), axis=axes) 
-    # mask_sum = np.sum(np.abs(y_true), axis=axes) + np.sum(np.abs(y_pred), axis=axes)
-    # union = mask_sum  - intersection 
-    
-    # smooth = .001
-    # iou = (intersection + smooth) / (union + smooth)
-    # return iou
 
 def mean_dice_np(y_true, y_pred, **kwargs):
     """
@@ -46,14 +35,14 @@ def mean_dice_np(y_true, y_pred, **kwargs):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test_path', type=str, default='/kaggle/working/npy_files', help='path to test dataset')
-    parser.add_argument('--save_path', type=str, default="/kaggle/working/results", help='path to save inference segmentation')
+    parser.add_argument('--test_path', type=str, default='npy_files', help='path to test dataset')
+    parser.add_argument('--save_path', type=str, default="results", help='path to save inference segmentation')
     parser.add_argument('--epoch', type=int, default=50, help='epoch for inference')
-    parser.add_argument('--model_path', type=str, default='/kaggle/working/models', help='path for testing models')
-    
+    parser.add_argument('--model_path', type=str, default='models', help='path for testing models')
+    parser.add_argument('--png_path', type=str, default='new_dataset/ISKEMI/test/MASKS')
     opt = parser.parse_args()
 
-    ckpt_path = os.path.join(opt.model_path, 'TransFuse_ISKEMI_' + str(opt.epoch) + '_Epoch.pth')
+    ckpt_path = os.path.join(opt.model_path, 'TransFuse_ISKEMI_' + str(opt.epoch) + '_Epoch_best.pth')
     
     model = TransFuse_S().cuda()
     model.load_state_dict(torch.load(ckpt_path))
@@ -74,6 +63,7 @@ if __name__ == '__main__':
     acc_bank = []
 
     for i in range(test_loader.size):
+        
         image, gt = test_loader.load_data()        
         gt = 1*(gt>0.5)
         image = image.cuda()
@@ -83,18 +73,20 @@ if __name__ == '__main__':
 
         res = res.sigmoid().data.cpu().numpy().squeeze()
         res = 1*(res > 0.5)
-
+        
+        gt_path = os.path.join(opt.png_path, str(i) + ".png")
+        res_path = os.path.join(opt.save_path, str(i) + ".png") 
+        
         if opt.save_path is not None:
-            imageio.imwrite(opt.save_path+'/'+str(i)+'_pred.jpg', res)
-            imageio.imwrite(opt.save_path+'/'+str(i)+'_gt.jpg', gt)
-
-        dice = mean_dice_np(gt, res)
-        iou = mean_iou_np(gt, res)
+            cv2.imwrite(res_path, res)
+        
+        dice = mean_dice_np(gt, res)           
+        iou = mean_iou_np(gt_path, res_path)
         acc = np.sum(res == gt) / (res.shape[0]*res.shape[1])
 
         acc_bank.append(acc)
         dice_bank.append(dice)
         iou_bank.append(iou)
 
-    print('Dice: {:.4f}, IoU: {:.4f}, Acc: {:.4f}'.
+    print('Dice: {:.4f}, IoU: {:f}, Acc: {:.4f}'.
         format(np.mean(dice_bank), np.mean(iou_bank), np.mean(acc_bank)))
